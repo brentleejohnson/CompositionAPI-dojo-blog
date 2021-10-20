efa5022ebc01a44bd80ccec2083165030efc3269
brentleejohnson <brentleejohnson73@gmail.com>
2021-10-20 09:36:26 +0200

    Added reactive values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="home">
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">Click me</button>
+    <button @click="age++">Add 1 to age</button>
+    <input type="text" v-model="name" />
   </div>
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
+
 export default {
   name: "Home",
   setup() {
-    let name = "mario";
-    let age = 30;
+    // const p = ref(null);
+
+    const name = ref("mario");
+    const age = ref(30);
 
     const handleClick = () => {
-      console.log("You clicked me");
+      name.value = "Luigi";
+      age.value = 35;
     };
 
     return { name, age, handleClick };
