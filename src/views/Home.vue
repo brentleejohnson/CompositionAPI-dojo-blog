95e0968f64157974750080f0bf10243c967c64a4
brentleejohnson <brentleejohnson73@gmail.com>
2021-10-20 10:55:03 +0200

    Life cycle hooks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,32 @@
 <template>
   <div class="home">
-    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">Click me</button>
-    <button @click="age++">Add 1 to age</button>
-    <input type="text" v-model="name" />
+    <h1>Home</h1>
+    <PostList v-if="showPosts" :posts="posts" />
+    <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">delete a post</button>
   </div>
 </template>
 
 <script>
-import { ref } from "@vue/reactivity";
-
+import { ref } from "vue";
+// component imports
+import PostList from "../components/PostList.vue";
 export default {
   name: "Home",
+  components: { PostList },
   setup() {
-    // const p = ref(null);
+    const posts = ref([
+      {
+        title: "welcome to the blog",
+        body:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in imperdiet nisi. Vestibulum eu dui dictum, ornare est non, dignissim dui. Donec at ex nulla. Aenean sem tortor, bibendum ac accumsan vel, elementum vel turpis. Nam auctor egestas pulvinar. Aenean placerat finibus finibus. Praesent dictum velit at purus aliquam, sit amet mattis est molestie. Duis a nulla et mauris luctus feugiat nec cursus odio. Nam elementum vitae est eu porttitor. Nulla vestibulum gravida magna eu ultricies. Nunc posuere tincidunt pellentesque. Sed id condimentum nisl, ultricies viverra orci. Fusce sit amet massa ut nibh feugiat elementum ac id est.",
+        id: 1,
+      },
+      { title: "top 5 CSS tips", body: "lorem ipsum", id: 2 },
+    ]);
+    const showPosts = ref(true);
 
-    const name = ref("mario");
-    const age = ref(30);
-
-    const handleClick = () => {
-      name.value = "Luigi";
-      age.value = 35;
-    };
-
-    return { name, age, handleClick };
+    return { posts, showPosts };
   },
 };
 </script>
