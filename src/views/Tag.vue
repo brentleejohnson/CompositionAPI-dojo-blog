278825b8748333991d6f30f49ef85bca561d7403
brentleejohnson <brentleejohnson73@gmail.com>
2021-10-21 13:36:07 +0200

    Added TagCloud

--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="tag">
     <div v-if="error">{{ error }}</div>
-    <div v-if="posts.length">
+    <div v-if="posts.length" class="layout">
       <PostList :posts="postsWithTag" />
+      <TagCloud :posts="posts" />
     </div>
     <div v-else>
       <Spinner />
@@ -11,27 +12,30 @@
 </template>
 
 <script>
+import TagCloud from "../components/TagCloud.vue";
 import Spinner from "../components/Spinner.vue";
 import PostList from "../components/PostList.vue";
 import getPosts from "../composables/getPosts";
 import { useRoute } from "vue-router";
 import { computed } from "vue";
-
 export default {
-  components: { PostList, Spinner },
+  components: { PostList, Spinner, TagCloud },
   setup() {
     const route = useRoute();
     const { posts, error, load } = getPosts();
-
     load();
-
     const postsWithTag = computed(() => {
       return posts.value.filter((p) => p.tags.includes(route.params.tag));
     });
-
     return { error, posts, postsWithTag };
   },
 };
 </script>
 
-<style></style>
+<style>
+.tag {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 10px;
+}
+</style>
